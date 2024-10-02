1db2101a367e0bc27913c225951e97ee5073206b
Pekka Klärck <peke@iki.fi>
2024-10-03 00:22:35 +0300

    API doc generation

---
+++ docs/api/generate.py
@@ -0,0 +1,55 @@
+"""Generate API documentation.
+
+API docs are part of the source code. This scripts generates references to
+them as well as navigation.
+
+Based on a `mkdocstrings` [recipe][] and a [script][] they use themselves.
+
+[recipe]: https://mkdocstrings.github.io/recipes/#automatic-code-reference-pages
+[script]: https://github.com/mkdocstrings/mkdocstrings/blob/main/scripts/gen_ref_nav.py
+"""
+
+from pathlib import Path
+
+import mkdocs_gen_files
+
+
+root = Path(__file__).parent.parent.parent
+src = root / 'src'
+nav = mkdocs_gen_files.Nav()
+
+# Add a navigation entry to the static `docs/api/index.md`.
+nav['api'] = 'index.md'
+
+
+for path in sorted(src.rglob('*.py')):
+    module = path.relative_to(src).with_suffix('').parts
+    doc_path = path.relative_to(src).with_suffix('.md')
+    full_doc_path = 'api' / doc_path
+
+    if module[-1] == '__init__':
+        module = module[:-1]
+        doc_path = doc_path.with_name('index.md')
+        full_doc_path = full_doc_path.with_name('index.md')
+    elif module[-1][0] == '_':
+        continue
+
+    # Add navigation entry for each module with decoration.
+    nav_parts = tuple(
+        f'<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code> {part}'
+        for part in module
+    )
+    nav[nav_parts] = doc_path.as_posix()
+
+    # Generate page with a reference.
+    with mkdocs_gen_files.open(full_doc_path, 'w') as file:
+        ident = '.'.join(module)
+        file.write(f'---\ntitle: {ident}\n---\n\n::: {ident}')
+
+    # Set correct edit path.
+    mkdocs_gen_files.set_edit_path(full_doc_path, '..' / path.relative_to(root))
+
+
+# Generate navigation file for `literal-nav`.
+with mkdocs_gen_files.open('api/nav.md', 'w') as file:
+    file.writelines(nav.build_literate_nav())
--- docs/api/index.md
+++ docs/api/index.md
@@ -1,5 +1,15 @@
 # Robot Framework API
 
-This section will be mostly generated.
+Robot Framework's primary, stable public API is provided by the [robot.api][]
+module. You may sometimes need to import items also from other modules, but
+that should be relatively rare.
 
-::: robot.run
+Command line entry points that can also be used programmatically are
+implemented by the following modules:
+
+  * [robot.run][] entry point for executing tests.
+  * `robot.rebot` entry point for post-processing outputs (Rebot).
+  * `robot.libdoc` entry point for the Libdoc tool.
+
+
+<!-- Rest of the content in the `api` directory is generated. -->
