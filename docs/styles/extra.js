b56928525d9d21f065f992434977820770a431e5
Pekka Klärck <peke@iki.fi>
2024-10-21 16:41:11 +0300

    Fine tune test/RPA toggle

--- docs/styles/extra.js
+++ docs/styles/extra.js
@@ -1,12 +1,15 @@
-function toggleTestOrTask(checked) {
+function toggleTestOrTask(rpa) {
+    const toggle = document.querySelector('#rpa-toggle input');
     const tests = document.getElementsByClassName('test');
     const tasks = document.getElementsByClassName('task');
-    if (checked) {
+    if (rpa) {
+        toggle.checked = true;
         setDisplay(tests, 'none');
         setDisplay(tasks, 'inline');
     } else {
         setDisplay(tests, 'inline');
         setDisplay(tasks, 'none');
+        toggle.checked = false;
     }
 }
 function setDisplay(elements, value) {
