5ec3056539ec48ecab1514c1b5eaedb4817f4bca
Pekka Klärck <peke@iki.fi>
2024-10-21 13:30:49 +0300

    New attempt to enable switching between testing and RPA modes
    The old approach used a Python-Markdown plugin for conversion and this
    one used a MkDocs hook. Both approaches work, but the former required
    the plugin to be importable.
    
    MkDocs doesn't seem to have way to add new directories to PYTHONPATH,
    and making the plugin installable would have been too much work. Hooks
    are loaded without extra configuration, and the MkDocs hoop API is
    also simpler than the Python-Markdownn plugin API.

---
+++ hooks/test_or_task.py
@@ -0,0 +1,36 @@
+"""Convert `{test}` to markup that can be rendered either as `test` or `task`.
+
+In practice `{test}` is converted to this:
+
+    <span class="robot-test">test</span><span class="robot-task">task</span>
+
+Finding the pattern is case-insentive, but the case is taken into account in
+conversion. For example, `{Test}` is converted to this:
+
+    <span class="robot-test">Test</span><span class="robot-task">Task</span>
+
+The idea is that external styles, possibly driven by Javascript, hide elements
+that have either the `robot-test` class or the `robot-task` class.
+"""
+
+import re
+
+
+def on_page_markdown(markdown, page, config, files):
+    return re.sub(r'(?i)\{(test)}', test_and_task, markdown)
+
+
+def test_and_task(match: re.Match) -> str:
+    test = match.group(1)
+    task = test_to_task(test)
+    return (f'<span class="robot-test">{test}</span>'
+            f'<span class="robot-task">{task}</span>')
+
+
+def test_to_task(test: str) -> str:
+    """Convert 'test' to 'task' so that case is taken into account.
+
+    For example, 'test' -> 'test' and 'Test' -> 'Task'.
+    """
+    lower = [c.islower() for c in test]
+    return ''.join(c if low else c.upper() for c, low in zip('task', lower))
