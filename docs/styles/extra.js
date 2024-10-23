6dcd81ed44379e334a3b823eeee8fe12fb3f18d0
Pekka Klärck <peke@iki.fi>
2024-10-23 17:45:21 +0300

    Make test/task toggle smaller on narrow screens.
    Show just the icon of the active mode.

--- docs/styles/extra.js
+++ docs/styles/extra.js
@@ -1,32 +1,51 @@
-// Show tests/tasks after the mode is changed. On page load styles are updated directly.
-function setTaskMode(enabled) {
-    setTaskDisplay(enabled);
-    setTaskHeaders(enabled);
-    document.querySelector('#rpa-toggle input').checked = enabled;
-    localStorage.setItem('robot-framework-manual-mode', enabled ? 'task' : 'test');
+const MODE_KEY = 'robot-framework-manual-mode';
+
+function toggleTestTaskMode() {
+    const mode = getNewMode();
+    setDisplay(mode);
+    setHeaders(mode);
+    setData(mode);
 }
 
-function setTaskDisplay(enabled) {
+function getNewMode() {
+    const mode = localStorage.getItem(MODE_KEY) == 'task' ? 'test' : 'task';
+    localStorage.setItem(MODE_KEY, mode);
+    return mode;
+}
+
+function setDisplay(mode) {
     for (const test of document.getElementsByClassName('test')) {
-        test.style.display = enabled ? 'none' : 'inline';
+        test.style.display = mode == 'test' ? 'inline' : 'none';
     }
     for (const task of document.getElementsByClassName('task')) {
-        task.style.display = enabled ? 'inline' : 'none';
+        task.style.display = mode == 'task' ? 'inline' : 'none';
     }
 }
 
-function setTaskHeaders(enabled) {
-    const newText = enabled ? '*** Tasks ***' : '*** Test Cases ***';
-    const oldText = enabled ? '*** Test Cases ***' : '*** Tasks ***';
-    for (const header of document.querySelectorAll('.language-robotframework .gh')) {
-        if (header.innerHTML == oldText) {
-            header.innerHTML = newText;
+function setHeaders(mode) {
+    const newHeader = mode == 'test' ? '*** Test Cases ***' : '*** Tasks ***';
+    const oldHeader = mode == 'test' ? '*** Tasks ***' : '*** Test Cases ***';
+    for (const elem of document.querySelectorAll('.language-robotframework .gh')) {
+        if (elem.innerHTML == oldHeader) {
+            elem.innerHTML = newHeader;
         }
     }
 }
 
+function setData(mode) {
+    if (mode == 'test') {
+        document.querySelector('#rpa-toggle input').checked = false;
+        document.getElementById('test-mode-icon').setAttribute('data-robot-narrow', 'show');
+        document.getElementById('task-mode-icon').setAttribute('data-robot-narrow', 'hide');
+    } else {
+        document.querySelector('#rpa-toggle input').checked = true;
+        document.getElementById('test-mode-icon').setAttribute('data-robot-narrow', 'hide');
+        document.getElementById('task-mode-icon').setAttribute('data-robot-narrow', 'show');
+    }
+}
+
 document$.subscribe(function () {
-    if (localStorage.getItem('robot-framework-manual-mode') == 'task') {
-        setTaskHeaders(true);
+    if (localStorage.getItem(MODE_KEY) == 'task') {
+        setHeaders('task');
     }
 });
