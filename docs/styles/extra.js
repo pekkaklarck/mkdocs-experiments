8e3718294554baeb730b48d98e09a0235117c2dd
Pekka Klärck <peke@iki.fi>
2024-10-23 15:10:39 +0300

    Store test/task mode.
    When the mode is changed:
    
    - The visibility of the already shown elements is updated.
    - Test/task headers are updated.
    - The current mode is stored to the localStorage.
    
    When a page is loaded:
    
    - CSS styles themselves are updated based on the localStorage. This
      makes items that are loaded to have the correct visibility from the
      beginning. No flickering!
    - Test/task headers are updated only after the pages has been loaded
      fully. There's some flickering and we need to investigating can this
      be enhanced.

--- docs/styles/extra.js
+++ docs/styles/extra.js
@@ -1,32 +1,32 @@
-function toggleTestOrTask(rpa) {
-    const toggle = document.querySelector('#rpa-toggle input');
-    const tests = document.getElementsByClassName('test');
-    const tasks = document.getElementsByClassName('task');
-    const headers = document.querySelectorAll('.language-robotframework .gh');
-    if (rpa) {
-        toggle.checked = true;
-        setDisplay(tests, 'none');
-        setDisplay(tasks, 'inline');
-    } else {
-        setDisplay(tests, 'inline');
-        setDisplay(tasks, 'none');
-        toggle.checked = false;
-    }
-    setHeader(headers, rpa);
+// Show tests/tasks after the mode is changed. On page load styles are updated directly.
+function setTaskMode(enabled) {
+    setTaskDisplay(enabled);
+    setTaskHeaders(enabled);
+    document.querySelector('#rpa-toggle input').checked = enabled;
+    localStorage.setItem('robot-framework-manual-mode', enabled ? 'task' : 'test');
 }
 
-function setDisplay(elements, value) {
-    for (const elem of elements) {
-        elem.style.display = value;
+function setTaskDisplay(enabled) {
+    for (const test of document.getElementsByClassName('test')) {
+        test.style.display = enabled ? 'none' : 'inline';
+    }
+    for (const task of document.getElementsByClassName('task')) {
+        task.style.display = enabled ? 'inline' : 'none';
     }
 }
 
-function setHeader(headers, rpa) {
-    const newText = rpa ? '*** Tasks ***' : '*** Test Cases ***';
-    const oldText = rpa ? '*** Test Cases ***' : '*** Tasks ***';
-    for (const elem of headers) {
-        if (elem.innerHTML == oldText) {
-            elem.innerHTML = newText;
+function setTaskHeaders(enabled) {
+    const newText = enabled ? '*** Tasks ***' : '*** Test Cases ***';
+    const oldText = enabled ? '*** Test Cases ***' : '*** Tasks ***';
+    for (const header of document.querySelectorAll('.language-robotframework .gh')) {
+        if (header.innerHTML == oldText) {
+            header.innerHTML = newText;
         }
     }
 }
+
+document$.subscribe(function () {
+    if (localStorage.getItem('robot-framework-manual-mode') == 'task') {
+        setTaskHeaders(true);
+    }
+});
--- docs/using/index.md
+++ docs/using/index.md
@@ -1,4 +1,10 @@
 # Using Robot Framework
 
 Introduction to the usage section. We have another references to the <keyword:>
-term here.
+term here. We are be talking about **{test}s** based on the test/task mode.
+
+```robotframework
+*** Test Cases ***
+Example
+    Log   Header should be set based on the test/task mode.
+```
